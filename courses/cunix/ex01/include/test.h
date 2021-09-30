da05c0813752dda74cc8043aacf2c182f94b3e48
pilot <vsvs2262asdfg@gmail.com>
2021-09-30 20:59:03 +0300

    week2

--- courses/cunix/ex01/include/test.h
+++
@@ -1,6 +0,0 @@
-#ifndef TEST_H_
-# define TEST_H_
-
-unsigned int  my_strlen(char *str);
-
-#endif
--- courses/cunix/ex04/include/test.h
+++
@@ -1,7 +0,0 @@
-#ifndef TEST_H_
-# define TEST_H_
-
-int my_atoi(const char *nptr);
-char* my_itoa(int nmb);
-
-#endif
