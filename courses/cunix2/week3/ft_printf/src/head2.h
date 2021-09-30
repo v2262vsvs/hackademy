df13294e71502743f43cb99dc8406ce9d925c499
pilot <vsvs2262asdfg@gmail.com>
2021-09-30 21:21:04 +0300

    week3

---
+++ courses/cunix2/week3/ft_printf/src/head2.h
@@ -0,0 +1,32 @@
+#ifndef HEAD2_H
+#define HEAD2_H
+
+#include <stdarg.h> // va_list 
+#include <stdlib.h> 
+#include <unistd.h> // write 
+
+
+char *(*get_func(char i))(va_list);
+char *create_buffer(void);
+void write_buffer(char *buffer, int len, va_list list);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
+//printf functions 
+int ft_printf(const char *format, ...);
+char *print_s(va_list list);
+char *print_c(va_list list);
+char *print_d(va_list list);
+
+
+
+typedef struct types
+{
+    char id;
+    char *(*func)(va_list);
+} print;
+
+#endif
+
+
+
---
+++ courses/cunix2/week3/ft_printf/src/strcpy.c
@@ -0,0 +1,13 @@
+#include "head2.h"
+char *_strcpy(char *dest, char *src)
+{
+	int x = 0;
+
+	while (src[x] != '\0')
+	{
+		dest[x] = src[x];
+		x++;
+	}
+	dest[x] = src[x];
+	return (dest);
+}
